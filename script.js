203066ce3ebca043aac64820957b13841da30e91
Holylogus <hamar.patrik94@gmail.com>
2023-03-18 10:07:36 +0100

    órai feladat mentés módosítás eőtt, kövi feladat kutyák létrehozása js-ben

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ window.addEventListener("load", init)
 
 function init(){
     const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
-    article = document.querySelectorAll("article")
-    article.innerHTML = "<button>Kutya</button>"
-    buttonElement.addEventListener("click", function(){
-        gomb(kutyaLista)
-    })
-}
+
+function init(){
+    const ARTICLE = document.querySelectorAll("article")
+    /*írjuk ki divekbe és azon belül p tagekbe a kutyák fajtáját*/
+
+    function osszeallit(){
+        //Összeállítjuk azt a tartalmat, ami a HTML kódot jelenti
+        const ELEM = 
+    }
 function gomb(lista){
     elem = document.querySelectorAll("button")
 
